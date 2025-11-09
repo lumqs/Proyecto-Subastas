7defb779dbfac02f40cafc95016ebcade2d78e4f
lumqs <lumqs.24@gmail.com>
2025-11-09 20:44:04 -0300

    nose que estoy cambiando pero confirmo

--- SubastasForms/Repository/UsuarioRepository.cs
+++ SubastasForms/Repository/UsuarioRepository.cs
@@ -36,7 +36,7 @@ namespace SubastasForms.Repository
         }
         public void EliminarUsuario(Usuario usuario)
         {
-            usuarios.Remove(usuario);//comentario de prueba
+            usuarios.Remove(usuario);//comentario de pruebaaa
         }
     }
 }
